0f70e5cb98a5f594bbe77365ace3595de03272a3
sycale <sankai1917@gmail.com>
2020-05-20 12:53:22 +0300

    sosi uje timur

--- src/task.js
+++ src/task.js
@@ -1,4 +1,10 @@
 import $ from 'jquery';
+import ko from 'knockout';
+
+const teachers = [];
+const docent = ko.observableArray([]);
+const prof = ko.observableArray([]);
+const others = ko.observableArray([]);
 
 function getUserInfo(user) {
   return $.ajax({
@@ -16,7 +22,37 @@ function getTeacherInfo(teacher) {
   });
 }
 
+function getTeachers() {
+  return $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: 'https://journal.bsuir.by/api/v1/employees',
+  });
+}
+
 $(document).ready(() => {
+  getTeachers().then(
+    (data) => {
+      console.log(data);
+      data.forEach((elem) => {
+        teachers.push(elem);
+        if (elem.rank === 'Профессор') {
+          prof.push(elem);
+        } else if (elem.rank === 'Доцент') {
+          docent.push(elem);
+        }
+        else if (elem.rank === null)
+        {
+          others.push(elem);
+        }
+      });
+      ko.applyBindings({ professors: prof, docent, others });
+    },
+    (errData) => {
+      console.log(errData);
+    },
+  );
+
   $('.btn').click(() => {
     $('.body').empty();
     const user = $('.us').val();
@@ -35,26 +71,36 @@ $(document).ready(() => {
       },
     );
   });
+
+  $('#start').click(() => {
+    $('.professors').removeClass('d-none');
+  });
+
   $('.teacher').click(() => {
     $('.body').empty();
     console.log('test');
     const teacher = $('.us').val();
-    getTeacherInfo(teacher).then((data) => {
-      console.log(data);
-      $('.body')
-        .append(`<h1>${data.employee.firstName} ${data.employee.lastName} ${data.employee.middleName}</h1> <br>
+    getTeacherInfo(teacher).then(
+      (data) => {
+        console.log(data);
+        $('.body')
+          .append(`<h1>${data.employee.firstName} ${data.employee.lastName} ${data.employee.middleName}</h1> <br>
       <img src = "${data.employee.photoLink}">
       `);
-      data.schedules.forEach((day) => {
-        $('.body').append(`<h1>${day.weekDay}: </h1><br>
+        data.schedules.forEach((day) => {
+          $('.body').append(`<h1>${day.weekDay}: </h1><br>
         
         `);
-        day.schedule.forEach((less) => {
-          $('.body').append(`<span> ${less.subject} : ${less.lessonType} - ${less.auditory} - ${less.lessonTime}  <br>`);
+          day.schedule.forEach((less) => {
+            $('.body').append(
+              `<span> ${less.subject} : ${less.lessonType} - ${less.auditory} - ${less.lessonTime}  <br>`,
+            );
+          });
         });
-      });
-    }, (errData) => {
-      console.log(errData);
-    });
+      },
+      (errData) => {
+        console.log(errData);
+      },
+    );
   });
 });
